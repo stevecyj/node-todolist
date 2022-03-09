57caf3f26f754d733ea394ec38808cb07ea7f13e
root <root@localhost.localdomain>
2022-03-09 18:21:31 +0800

    [Feature]:uuid

--- server.js
+++ server.js
@@ -1,33 +1,35 @@
-const http = require('http');
+const http = require("http");
+const { v4: uuidv4 } = require("uuid");
+const todos = [];
 
 const requestListener = (req, res) => {
-  console.log(req.url + '\n', req.method);
+  console.log(req.url + "\n", req.method);
   const headers = {
-    'Access-Control-Allow-Headers':
-      'Content-Type, Authorization, Content-Length, X-Requested-With',
-    'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Methods': 'PATCH, POST, GET,OPTIONS,DELETE',
-    'Content-Type': 'application/json',
+    "Access-Control-Allow-Headers":
+      "Content-Type, Authorization, Content-Length, X-Requested-With",
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "PATCH, POST, GET,OPTIONS,DELETE",
+    "Content-Type": "application/json",
   };
 
-  if (req.url == '/' && req.method == 'GET') {
+  if (req.url == "/todos" && req.method == "GET") {
     res.writeHead(200, headers);
     res.write(
       JSON.stringify({
-        status: 'success',
-        data: [],
+        status: "success",
+        data: todos,
       })
     );
     res.end();
-  } else if (req.method == 'OPTIONS') {
+  } else if (req.method == "OPTIONS") {
     res.writeHead(200, headers);
     res.end();
   } else {
     res.writeHead(404, headers);
     res.write(
       JSON.stringify({
-        status: 'false',
-        messate: 'no route for this site',
+        status: "false",
+        messate: "no route for this site",
       })
     );
     res.end();
