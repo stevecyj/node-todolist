3954d7f5b3f79e9562184a827d874fc948491ca4
stevecyj <ycsteven.tsao@gmail.com>
2022-03-09 13:44:42 +0800

    adj headers info

--- server.js
+++ server.js
@@ -2,19 +2,31 @@ const http = require('http');
 
 const requestListener = (req, res) => {
   console.log(req.url + '\n', req.method);
-  const headers = { 'Content-Type': 'text/plain' };
+  const headers = {
+    'Access-Control-Allow-Headers':
+      'Content-Type, Authorization, Content-Length, X-Requested-With',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'PATCH, POST, GET,OPTIONS,DELETE',
+    'Content-Type': 'application/json',
+  };
 
   if (req.url == '/' && req.method == 'GET') {
     res.writeHead(200, headers);
-    res.write('index');
-    res.end();
-  } else if (req.url == '/' && req.method == 'DELETE') {
-    res.writeHead(200, headers);
-    res.write('delete');
+    res.write(
+      JSON.stringify({
+        status: 'success',
+        data: [],
+      })
+    );
     res.end();
   } else {
     res.writeHead(404, headers);
-    res.write('not found 404');
+    res.write(
+      JSON.stringify({
+        status: 'false',
+        messate: 'no route for this site',
+      })
+    );
     res.end();
   }
 };
